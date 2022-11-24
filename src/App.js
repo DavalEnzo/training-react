6f390d48927323b50aa05a9b043074328fc68101
DavalEnzo <davalenzo@gmail.com>
2022-11-24 17:49:18 +0100

    Ajout de tailwindcss

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
     if (isLoading) {
         return <div className="App-header">
             <Button>
-                <Spinner color="blue" size="xl" aria-label="Spinner button example"/>
+                <Spinner color="success" size="xl" aria-label="Spinner button example"/>
                 <span className="pl-3">
       Chargement des données
     </span>
@@ -45,16 +45,41 @@ function App() {
                 <div>
                 </div>
                 <header className="App-header">
-                    <div>
+                    <div className="group">
                         <button type="button"
                                 className="inline-flex relative items-center pl-3 pr-1 py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Messages
+                            Messages
                             <span className="flex h-3 w-3">
-  <span className="animate-ping inline-flex absolute -top-1 -right-1 justify-center items-center h-1/3 w-1/5 rounded-full bg-sky-400 opacity-75"></span>
-  <span className="inline-flex absolute -top-1 -right-1 justify-center items-center w-5 h-5 bg-sky-500 rounded-full"> 2</span>
-</span>
+                              <span
+                                  className="animate-ping inline-flex absolute -top-1 -right-1 justify-center items-center h-1/3 w-3/12 rounded-full bg-sky-400 opacity-75"></span>
+                              <span style={{animationFillMode: "both"}}
+                                    className="inline-flex group-hover:rotate-360 group-hover:duration-1000 absolute -top-1 -right-1 justify-center items-center w-5 h-5 bg-sky-500 rounded-full"> 2</span>
+                            </span>
                         </button>
                     </div>
+
+                    <div
+                        className="max-w-3xl relative pl-36 my-12 overflow-hidden p-6 border 0 rounded-lg shadow-md bg-gray-800 border-gray-700">
+                        <a href="#">
+                            <img className="absolute -left-6 -top-5" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt=""/>
+                            <p className="absolute -left-0 text-base font-medium top-32">Rédigé par raphaël</p>
+                            <h5 className="mb-2 text-2xl text-center font-bold tracking-tight text-white">Noteworthy
+                                technology acquisitions 2021</h5>
+                        </a>
+                        <p className="mb-3 font-normal text-gray-400">Here are the biggest enterprise
+                            technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                        <a href="#"
+                           className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            Read more
+                            <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor"
+                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd"
+                                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                      clipRule="evenodd"></path>
+                            </svg>
+                        </a>
+                    </div>
+
 
                     <p>
                         {data.label} <br></br>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      rotate: {
+        '360': '360deg',
+      },
+    },
   },
   plugins: [
       require('flowbite/plugin')
